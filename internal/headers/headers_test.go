dd030fb95605908c415bbadc140125e1bbd8b213
Iosaf-uwu <116690709+Iosaf-uwu@users.noreply.github.com>
2026-01-19 21:55:11 -0700

    Ch5Lesson2: Normalize header keys and validate allowed characters
    Always lowercase header keys and updated tests accordingly
    Validate header keys against the allowed character set and return errors for invalid keys

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeaders_Parse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -26,24 +26,32 @@ func TestHeaders_Parse(t *testing.T) {
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
+	// Test: Invalid character in header
+	headers = NewHeaders()
+	data = []byte("       H@st : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
 	//Valid single header with extra whitespace
 	headers = NewHeaders()
-	data = []byte("     Host: localhost:42069   \r\n\r\n")
+	data = []byte("     HosT: localhost:42069   \r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 31, n)
 	assert.False(t, done)
 
 	// Valid 2 headers with existing headers
 	headers = NewHeaders()
-	data = []byte("Host: localhost:42069\r\nFriend:mauricio\r\n")
+	data = []byte("HOst: localhost:42069\r\nFriend:mauricio\r\n")
 
 	// First header
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	assert.False(t, done)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 
 	// advance buffer
 	data = data[n:]
@@ -52,7 +60,7 @@ func TestHeaders_Parse(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	assert.False(t, done)
-	assert.Equal(t, "mauricio", headers["Friend"])
+	assert.Equal(t, "mauricio", headers["friend"])
 
 	// Valid done
 	headers = NewHeaders()
