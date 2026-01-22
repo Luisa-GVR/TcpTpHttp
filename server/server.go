c68708d513c4b306eda1c783f0712d270f1ba508
Iosaf-uwu <116690709+Iosaf-uwu@users.noreply.github.com>
2026-01-22 16:19:32 -0700

    Ch7Lesson1: feat(httpserver): add listener based HTTP server

---
+++ server/server.go
@@ -0,0 +1,74 @@
+package server
+
+import (
+	"log"
+	"net"
+	"strconv"
+	"sync/atomic"
+)
+
+type Server struct {
+	isRunning atomic.Bool
+	Listen    net.Listener
+}
+
+func Serve(port int) (*Server, error) {
+
+	l, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return &Server{}, err
+	}
+
+	s := new(Server)
+	s.isRunning.Store(true)
+	s.Listen = l
+
+	go func() {
+		s.listen()
+	}()
+
+	return s, nil
+}
+
+func (s *Server) Close() error {
+	s.isRunning.Store(false)
+
+	err := s.Listen.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) listen() {
+	for {
+		if !s.isRunning.Load() {
+			return
+		}
+
+		conn, err := s.Listen.Accept()
+		if err != nil {
+
+			if s.isRunning.Load() {
+				log.Println(err)
+				continue
+			} else {
+				return
+			}
+
+		}
+
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+
+	str := "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 13\n\nHello World!"
+	bytes := []byte(str)
+
+	conn.Write(bytes)
+	defer conn.Close()
+
+}
