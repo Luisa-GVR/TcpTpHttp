cc60757b1fbee8da363cf025f5a1570d3061d465
Iosaf-uwu <116690709+Iosaf-uwu@users.noreply.github.com>
2026-01-22 20:43:04 -0700

    Ch7Lesson5: feat(server): implement handler pattern with error handling

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"httpfromtcp/internal/request"
+	"httpfromtcp/internal/response"
 	"httpfromtcp/server"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -11,7 +14,26 @@ import (
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+
+	handler := func(w io.Writer, req *request.Request) *server.HandlerError {
+		switch req.RequestLine.RequestTarget {
+		case "/yourproblem":
+			return &server.HandlerError{
+				StatusCode: response.Code400,
+				Message:    "Your problem is not my problem\n",
+			}
+		case "/myproblem":
+			return &server.HandlerError{
+				StatusCode: response.Code500,
+				Message:    "Woopsie, my bad\n",
+			}
+		default:
+			w.Write([]byte("All good, frfr\n"))
+			return nil
+		}
+	}
+
+	server, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
